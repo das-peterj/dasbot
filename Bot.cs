002e0e0c81639abb8d7d7e507d47ba099c9868c5
Peter Jorgensen <dasbomber.jorgensen@gmail.com>
2022-04-10 20:08:51 +0200

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_Dasbot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = sr.ReadToEnd();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug,
+                // Have not managed to find a solution for this
+                //UseInternalLogHandler = true,
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] {configJson.Prefix},
+                EnableDms = false,
+                EnableMentionPrefix = true
+            };
+
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DiscordBot_Dasbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
